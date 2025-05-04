33eb9a7c954b0dd345848677f25db0a721d08dab
Eldar Khuzin <khuzin.e@gmail.com>
2025-05-04 16:48:54 +0000

    Train model on 20 basins

--- code/src/newutils/data/datasets.py
+++ code/src/newutils/data/datasets.py
@@ -14,7 +14,9 @@ def load_train_subset():
     Returns:
         dask.dataframe: dataframe of readed files.
     """
-    train_df = pl.read_csv(PATH_MERGED_DATASETS / FILENAME_TRAIN_IDS).sample(8, seed=30)
+    train_df = pl.read_csv(PATH_MERGED_DATASETS / FILENAME_TRAIN_IDS).sample(
+        20, seed=30
+    )
     file_ids = train_df["file_id"].to_list()
     paths = [PATH_MERGED_DATASETS / f"{file_id}.parquet" for file_id in file_ids]
     return dd.read_parquet(paths).set_index(COL_GAUGE_ID)
